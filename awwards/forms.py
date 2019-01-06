955f2805f28aa6e4639df61606f19b021141037f
hamida-mstafa <hamidamstafa@gmail.com>
2019-01-06 19:52:05 +0300

    registered a signup form

--- awwards/forms.py
+++ awwards/forms.py
@@ -1,48 +1,54 @@
-from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from .models import Profile,Posts,Comments,Likes
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+# from .models import Profile,Posts,Comments,Likes
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
+    first_name = forms.CharField(max_length=40)
+    last_name = forms.CharField(max_length=40)
 
     class Meta:
         model = User
         exclude = []
         fields = ['first_name','last_name','username','email','password1','password2']
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Posts
-        exclude = ['user']
-        # fields = ['']
-
-class PostsForm(forms.ModelForm):
-    class Meta:
-        model = Posts
-        exclude = ['user']
-        fields = ['name','link','image1','image2','image3','video']
-
-class Comments(forms.ModelForm):
-    class Meta:
-        model = Comments
-        exclude = []
-        fields = ['comment']
-
-
-rates_choices = [
-    (1,'1'),
-    (2,'2'),
-    (3,'3'),
-    (4,'4'),
-    (5,'5'),
-]
-
-class Votes(forms.Form):
-    design = forms.CharField(label='Design level', widget=forms.RadioSelect(choices=rates_choices))
-
-    usability = forms.CharField(label='Usability level', widget=forms.RadioSelect(choices=rates_choices))
-
-    creativity  = forms.CharField(label='Creativity level', widget=forms.RadioSelect(choices=rates_choices))
-
-    content = forms.CharField(label='Content level', widget=forms.RadioSelect(choices=rates_choices))
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         exclude = ['user']
+#         fields = ['dp','bio']
+#
+# class PostsForm(forms.ModelForm):
+#     class Meta:
+#         model = Posts
+#         exclude = ['user']
+#         fields = ['name','link','image1','image2','image3','video']
+#
+#
+# class Comments(forms.ModelForm):
+#     class Meta:
+#         model=Comments
+#         exclude=[]
+#         fields=['comment']
+#
+#
+# rating_choices = [
+#     (1, '1'),
+#     (2, '2'),
+#     (3, '3'),
+#     (4, '4'),
+#     (5, '5'),
+#     (6, '6'),
+#     (7, '7'),
+#     (8, '8'),
+#     (9, '9'),
+#     (10, '10'),
+# ]
+#
+# class Votes(forms.Form):
+#     design = forms.CharField(label='Design level', widget=forms.RadioSelect(choices=rating_choices))
+#
+#     usability = forms.CharField(label='Usability level', widget=forms.RadioSelect(choices=rating_choices))
+#
+#     creativity  = forms.CharField(label='Creativity level', widget=forms.RadioSelect(choices=rating_choices))
+#
+#     content = forms.CharField(label='Content level', widget=forms.RadioSelect(choices=rating_choices))
