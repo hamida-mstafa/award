85ec4fa57faa2f5b6619ae680d3f9897003b336e
hamida-mstafa <hamidamstafa@gmail.com>
2019-01-06 19:41:04 +0300

    defined the post methods

--- awwards/models.py
+++ awwards/models.py
@@ -1,15 +1,16 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.core.validators import MinValueValidator,MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
-
 # Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-    dp = models.ImageField(upload_to='images')
-    bio = HTMLField(max_length=300)
+    dp =  models.ImageField(upload_to='images')
+    bio = HTMLField(max_length=500)
     phone_number = models.BigIntegerField()
 
     def save_profile(self):
@@ -21,9 +22,10 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Posts(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
     link = models.URLField()
     image1 = models.ImageField(upload_to='images')
     image2 = models.ImageField(upload_to='images')
@@ -38,92 +40,30 @@ class Posts(models.Model):
     class Meta:
         ordering = ['-id']
 
-#     @classmethod
-#     def save_post(self):
-#         self.save()
-#     @classmethod
-#     def get_posts(cls):
-#          posts = cls.objects.all()
-#          return posts
-#     @classmethod
-#     def delete_post(self):
-#         self.delete()
-#
-#     def save_image(self):
-#         self.save()
-#
-#     @classmethod
-#     def delete_image_by_id(cls, id):
-#         pictures = cls.objects.filter(pk=id)
-#         pictures.delete()
-#
-#     @classmethod
-#     def get_im# -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
-#
-# from django.core.validators import MinValueValidator,MaxValueValidator
-# from django.db import models
-# from django.contrib.auth.models import User
-# from tinymce.models import HTMLField
-# # Create your models here.
-#
-# class Profile(models.Model):
-#     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
-#     dp =  models.ImageField(upload_to='images')
-#     bio = HTMLField(max_length=500)
-#     phone_number = models.BigIntegerField()
-#
-#     def save_profile(self):
-#         self.save()
-#
-#     def delete_profile(self):
-#         self.delete()
-#
-#     def __str__(self):
-#         return self.user.username
-#
-#
-# class Posts(models.Model):
-#     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
-#     name = models.CharField(max_length=50)
-#     link = models.URLField()
-#     image1 = models.ImageField(upload_to='images')
-#     image2 = models.ImageField(upload_to='images')
-#     image3 = models.ImageField(upload_to='images')
-#     postedon = models.DateTimeField(auto_now_add=True)
-#     video = models.FileField(upload_to='videos',null=True)
-#
-#
-#     def __str__(self):
-#         return self.user.username
-#
-#     class Meta:
-#         ordering = ['-id']
-#
-#     @classmethod
-#     def save_post(self):
-#         self.save()
-#     @classmethod
-#     def get_posts(cls):
-#          posts = cls.objects.all()
-#          return posts
-#     @classmethod
-#     def delete_post(self):
-#         self.delete()
-#
-#     def save_image(self):
-#         self.save()
-#
-#     @classmethod
-#     def delete_image_by_id(cls, id):
-#         pictures = cls.objects.filter(pk=id)
-#         pictures.delete()
-#
-#     @classmethod
-#     def get_image_by_id(cls, id):
-#         pictures = cls.objects.get(pk=id)
-#         return pictures
-#
+    @classmethod
+    def save_post(self):
+        self.save()
+    @classmethod
+    def get_posts(cls):
+         posts = cls.objects.all()
+         return posts
+    @classmethod
+    def delete_post(self):
+        self.delete()
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def delete_image_by_id(cls, id):
+        pictures = cls.objects.filter(pk=id)
+        pictures.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        pictures = cls.objects.get(pk=id)
+        return pictures
+
 # class Comments(models.Model):
 #     user = models.ForeignKey(User)
 #     post=models.ForeignKey(Posts,related_name='comments')
@@ -135,19 +75,4 @@ class Posts(models.Model):
 #     design = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
 #     usability = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
 #     creativity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
-#     content = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])age_by_id(cls, id):
-#         pictures = cls.objects.get(pk=id)
-#         return pictures
-#
-# class Comments(models.Model):
-#     user = models.ForeignKey(User)
-#     post = models.ForeignKey(Posts,related_name='comments')
-#     comment = models.CharField(max_length=150)
-#
-# class Likes(models.Model):
-#     user = models.ForeignKey(User,on_delete=models.CASCADE)
-#     post =  models.ForeignKey(Posts,on_delete=models.CASCADE,related_name='likes')
-#     design = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-#     usability = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-#     creativity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-#     content = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+#     content = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
